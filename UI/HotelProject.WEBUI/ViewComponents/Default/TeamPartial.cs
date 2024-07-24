92daa88494a43c6aa5107c71e4845814e1edac38
Oğuzhan Savaş <mustafaoguzhan.savas@bilgeadamboost.com>
2024-07-24 11:59:33 +0300

    project ui and view components added

---
+++ UI/HotelProject.WEBUI/ViewComponents/Default/TeamPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HotelProject.WEBUI.ViewComponents.Default
+{
+    public class TeamPartial : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
---
+++ UI/HotelProject.WEBUI/ViewComponents/Default/VideoPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HotelProject.WEBUI.ViewComponents.Default
+{
+    public class VideoPartial : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
