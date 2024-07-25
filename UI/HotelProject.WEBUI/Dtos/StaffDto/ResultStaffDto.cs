f735853cd4f01039868509a9a939bf7256602c2e
Oğuzhan Savaş <mustafaoguzhan.savas@bilgeadamboost.com>
2024-07-25 11:56:50 +0300

    staff api consume

---
+++ UI/HotelProject.WEBUI/Dtos/StaffDto/ResultStaffDto.cs
@@ -0,0 +1,13 @@
+﻿namespace HotelProject.WEBUI.Dtos.StaffDto
+{
+    public class ResultStaffDto
+    {
+        public int StaffId { get; set; }
+        public string Name { get; set; }
+        public string Title { get; set; }
+
+        public string SocialMedia1 { get; set; }
+        public string SocialMedia2 { get; set; }
+        public string SocialMedia3 { get; set; }
+    }
+}
--- UI/HotelProject.WEBUI/ViewComponents/Default/TeamPartial.cs
+++ UI/HotelProject.WEBUI/ViewComponents/Default/TeamPartial.cs
@@ -1,11 +1,27 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using HotelProject.WEBUI.Dtos.StaffDto;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 
 namespace HotelProject.WEBUI.ViewComponents.Default
 {
     public class TeamPartial : ViewComponent
     {
-        public IViewComponentResult Invoke()
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public TeamPartial(IHttpClientFactory httpClientFactory)
         {
+            _httpClientFactory = httpClientFactory;
+        }
+        public async  Task<IViewComponentResult> InvokeAsync()
+        {
+            var cleint = _httpClientFactory.CreateClient();
+            var responseMessage = await cleint.GetAsync("https://localhost:7243/api/Staff");
+            if(responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultStaffDto>>(jsonData);
+                return View(values);
+            }
             return View();
         }
     }
