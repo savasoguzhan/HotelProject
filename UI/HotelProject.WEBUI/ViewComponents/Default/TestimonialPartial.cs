cfee5e88db39961ee83c630bd71d7a97f224c6aa
Oğuzhan Savaş <mustafaoguzhan.savas@bilgeadamboost.com>
2024-07-25 11:41:31 +0300

    Testimonial API Consume

--- UI/HotelProject.WEBUI/ViewComponents/Default/TestimonialPartial.cs
+++ UI/HotelProject.WEBUI/ViewComponents/Default/TestimonialPartial.cs
@@ -1,11 +1,27 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using HotelProject.WEBUI.Dtos.TestimonialDto;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 
 namespace HotelProject.WEBUI.ViewComponents.Default
 {
     public class TestimonialPartial : ViewComponent
     {
-        public IViewComponentResult Invoke()
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public TestimonialPartial(IHttpClientFactory httpClientFactory)
         {
+            _httpClientFactory = httpClientFactory;
+        }
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7243/api/Testimonial");
+            if(responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultTestimonialDto>>(jsonData);
+                return View(values);
+            }
             return View();
         }
     }
